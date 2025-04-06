2c358ac79f8856494d3de697912296f8bef9d312
Luis Hernan <ramirezluishernan5@gmail.com>
2025-04-05 19:01:49 -0600

    modificaciondb

--- Proyecto/src/config/db.js
+++ Proyecto/src/config/db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
     host: process.env.DB_HOST,  
     user: process.env.DB_USER,  
     password: process.env.DB_PASSWORD,  
-    database: process.env.DB_NAME,  // Corregido: "datebase" → "database"
+    database: process.env.DB_NAME,  // Corregido:"datebase" → "database"
     port: process.env.DB_PORT,  
     ssl: {  
         rejectUnauthorized: true,  
